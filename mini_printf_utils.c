66a8c27edf153ceadaef0ebadbd79cb363606042
Ayman Abourizq <aabouriz@c4r5p6.1337.ma>
2024-12-07 21:33:12 +0100

    LINUX

--- mini_printf_utils.c
+++ mini_printf_utils.c
@@ -1,2 +1,17 @@
 #include "mini_printf.h"
 
+int	ft_putchar(char c)
+{
+	return ((int)write(1, &c, 1));
+}
+
+int	ft_putnbr(int n)
+{
+	int	ret;
+
+	if (n == -2147483648)
+		return ((int)write(1, "-2147483648", 11));
+	ret = 0;
+	if (n < 0)
+		;
+}
